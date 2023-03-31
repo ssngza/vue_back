e4dc8b9d5f32d9dd68c841583cb12ec871d53484
김승현 <ksh534700@gmail.com>
2023-03-31 21:09:15 +0900

    23.03.31 #1 외주관리서비스 외주조회 컨트롤러 수정(김승현)

--- src/main/java/kr/co/seoulit/erp/logistic/outsourcing/controller/OutsourcOrderController.java
+++ src/main/java/kr/co/seoulit/erp/logistic/outsourcing/controller/OutsourcOrderController.java
@@ -6,12 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import kr.co.seoulit.erp.logistic.outsourcing.servicefacade.OutsourcServiceFacade;
 import kr.co.seoulit.erp.logistic.outsourcing.to.OutsourcTO;
 
@@ -25,20 +20,23 @@ public class OutsourcOrderController {
 
 	private ModelMap modelMap = new ModelMap();
 
-	@RequestMapping(value = "/searchOderableList", method = RequestMethod.GET)
-	public HashMap<String, Object> searchOderableList(@RequestParam String searchDateCondition,
-			@RequestParam String startDate, @RequestParam String endDate) {
+	@GetMapping(value = "searchOderableList")
+	public HashMap<String, Object> searchOderableList(
+			@RequestParam String startDate,
+			@RequestParam String endDate) {
 		HashMap<String, Object> resultMap = new HashMap<>();
-		try {
-			resultMap.put("gridRowJson", outsourcSF.searchMrpGatheringList(searchDateCondition, startDate, endDate));
-			resultMap.put("errorCode", 1);
-			resultMap.put("errorMsg", "성공");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			resultMap.put("errorCode", -2);
-			resultMap.put("errorMsg", e.getMessage());
-		}
+		System.out.println(startDate);
+		System.out.println(endDate);
+//		try {
+//			resultMap.put("gridRowJson", outsourcSF.searchMrpGatheringList(searchDateCondition, startDate, endDate));
+//			resultMap.put("errorCode", 1);
+//			resultMap.put("errorMsg", "성공");
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			resultMap.put("errorCode", -2);
+//			resultMap.put("errorMsg", e.getMessage());
+//		}
 		return resultMap;
 	}
 
